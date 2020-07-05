6c2932ff816b936a588bdaa597aa88e187aa146e
Tischa <tischa.nyanguile@student.ehb.be>
2020-07-05 17:04:53 +0200

    Implemented store functionality

--- user-management-system-app/app/Http/Controllers/UserController.php
+++ user-management-system-app/app/Http/Controllers/UserController.php
@@ -34,7 +34,20 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'first_name'=>'required',
+            'last_name'=>'required',
+            'email'=>'required'
+        ]);
+        $contact = new Contact([
+            'first_name' => $request->get('first_name'),
+            'last_name' => $request->get('last_name'),
+            'email' => $request->get('email'),
+            'job_title' => $request->get('job_title'),
+            'city' => $request->get('city'),
+            'country' => $request->get('country')
+        ]);
+        $contact->save();
     }
 
     /**
